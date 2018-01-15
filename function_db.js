3dfdcb0f316bc55967eaf7b10360e0b1572b5be0
dssd <gonza.edelp77@gmail.com>
2018-01-15 19:25:29 -0300

    first commit

---
+++ function_db.js
@@ -0,0 +1,136 @@
+
+// --------------- Functions for obtain data from database -----------------------
+
+function busqueda_usuario(loggedName,user_id,callback){ //CAMBIAR!!!
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-east-1'});
+    var params = {
+          TableName : 'User',
+          ProjectionExpression: "#name",
+          FilterExpression : '#name = :n',
+          ExpressionAttributeNames: {'#name':'name'},
+          ExpressionAttributeValues : {':n' : loggedName}
+    };
+
+    var documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+    console.log('Buscando un item en la tabla user por: userId,name ');
+
+    documentClient.scan(params, function(err, data) {
+       if (err) console.log(err);
+       else{ 
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data.Items.length != 0){ //si encontró el nombre
+            data.Items.forEach(function(row){
+                //console.log("----entro al for each " + row.name);
+                callback(row.name); //retorna el nombre del usuario 
+            });
+        } else{
+           callback(null); 
+        }
+       }
+    });
+    
+ 
+}
+
+function registrar_usuario(username,user_id){ 
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-east-1'});
+    var params = {
+      TableName : 'User',
+      Item: {
+         userId: user_id,
+         name:username
+      }
+    };
+
+    var documentClient = new AWS.DynamoDB.DocumentClient();
+
+    documentClient.put(params, function(err, data) {
+      if (err) console.log(err);
+      else console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
+
+    });
+        
+ 
+}
+
+function obtener_datos_conf(loggedName,user_id,callback){ //CAMBIAR!!!
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-east-1'});
+    
+
+    var params = {
+          TableName : 'User',
+          ProjectionExpression: "#url, clase",
+          FilterExpression : '#name = :n',
+          ExpressionAttributeNames: {'#name':'name','#url':'url'},
+          ExpressionAttributeValues : {':n' : loggedName}
+    };
+
+    var documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+    console.log('Buscando un item en la tabla user por: userId,name ');
+
+    documentClient.scan(params, function(err, data) {
+       if (err) console.log(err);
+       else{ 
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data.Items.length != 0){ //si encontró el nombre
+            data.Items.forEach(function(row){
+                //console.log("----entro al for each " + row.name);
+                callback(row.url,row.clase); //retorna la url y la clase obtenidas de la configuracion del usuario 
+            });
+        } else{
+           callback(null,null); 
+        }
+       }
+    });
+}
+
+exports.busqueda_usuario = busqueda_usuario;
+exports.registrar_usuario = registrar_usuario;
+exports.obtener_datos_conf = obtener_datos_conf;
+
+
+
+/*            var params = {
+        TableName: 'User',
+        Key:{
+            'userId': user_id,
+            'name':loggedName
+        }
+        };
+        console.log('------ params'+ params.Key.userId + params.Key.name);
+     var params = {
+          ExpressionAttributeNames: {
+           "AT": "AlbumTitle", 
+           "ST": "SongTitle"
+          }, 
+          ExpressionAttributeValues: {
+           ":a": {
+             S: "No One You Know"
+            }
+          }, 
+          FilterExpression: "Artist = :a", 
+          ProjectionExpression: "#ST, #AT", 
+          TableName: "Music"
+         }; 
+*/
+
+/*
+    docClient.get(params, (err, data) => {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+
+            callback(data.Item.nuevo);  //retorna el nombre del usuario si es que está
+
+        }
+    });
+*/
---
+++ index.js
@@ -0,0 +1,228 @@
+var Alexa = require('alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.6a7efb87-1f31-49dd-b07a-0c6ee7993a0e';
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context, callback) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+        var alexa = Alexa.handler(event, context, callback);
+        alexa.appId = APP_ID;
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        //alexa.dynamoDBTableName = 'User';
+        if (event.session.application.applicationId !== APP_ID) {
+             context.fail("Invalid Application ID");
+         }
+       
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        alexa.registerHandlers(handlers);
+        alexa.execute();
+    
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+}; //End exports.handler
+
+//Funciones de interaccion con la base
+var func_db = require('./function_db'); 
+
+var readModeHandlers = {
+
+};
+var startModeHandlers = {
+    'LaunchRequest': function () {
+        this.response.speak('Welcome to Read Notice Skill. Do yo want to start?')
+        .listen('Say yes to start or no to quit.');
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        //onSessionEnded(this.event.request, this.event.session);
+        this.emit(':tell','Goodbye!');
+    },
+    'AMAZON.YesIntent': function(){
+        this.response.speak('Great! Please try to saying the utterance for login in the system')
+        .listen('Please say: sign in with name');
+        this.emit(':responseReady');
+    },
+    'AMAZON.NoIntent': function(){
+        this.emit(':tell','Ok, see you next time');
+    },
+};
+
+
+//Handlers para los intent del skill
+var handlers = {
+    'LaunchRequest': function () {
+        this.response.speak('Welcome to Read Notice Skill. Do yo want to start?')
+        .listen('Say yes to start or no to quit.');
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        //onSessionEnded(this.event.request, this.event.session);
+        this.emit(':tell','Goodbye!');
+    },
+    'ReturnNoticeIntent': function () {
+        this.emit('Read');
+    },
+    'DialogIntent':function(){
+
+    },
+    'Register': function(){
+        //var myThis = this;
+        var slotValue = this.event.request.intent.slots.User.value;//Obtengo el usuario
+        func_db.busqueda_usuario(slotValue,this.event.session.user.userId,(username)=>{
+
+            if(username == null){//Si no está registrado en la base con ese nombre
+                func_db.registrar_usuario(slotValue,this.event.session.user.userId);       
+                this.emit(':tell', "Great! Could be successfully registered with the username "+ slotValue);
+            }        
+            else{
+                this.emit(':tell', "Sorry, the user already exists. It is not possible to register with the username "+ slotValue +". Please, register with a new name");
+            }
+        });
+    },
+    'Login': function(){
+        //var myThis = this;
+        var slotValue = this.event.request.intent.slots.User.value;//Obtengo el usuario
+        if(!this.attributes['logueado']){ //Si no ingresó nadie todavia
+            func_db.busqueda_usuario(slotValue,this.event.session.user.userId,(username)=>{ //CAMBIAR!!!
+                if(username != null){//Si ya está registrado en la base        
+                    this.attributes['logueado']= slotValue; //Se loguea: Asigna a prop.'logueado' de attributes de session el usuario del slot
+                    this.emit(':tell', "Hello "+ this.attributes['logueado']+"! Now, you can get your content");
+                }
+                else{
+                    this.emit(':tell', "Sorry, there is no registered user with the name "+ slotValue +". Please, log in with a valid username");
+                }
+            });
+        }
+        else{
+            this.emit(':tell', "Sorry, you can not login because the user "+ this.attributes['logueado']+" is already active in the system");
+        }
+    },
+    'Logout': function(){
+        var slotValue = this.event.request.intent.slots.User.value;//Obtengo el usuario
+            if(this.attributes['logueado'] == slotValue){ //Si existe un usuario activo con el nombre del slotValue
+                this.attributes['logueado']= ""; //Inicializa a prop.'logueado' de attributes de session 
+                this.emit(':tell', "Goodbye "+ slotValue+". See you later!");
+            }
+            else if(this.attributes['logueado'] == ""){
+                this.emit(':tell', "Sorry, you must first login before you can logout");
+            } 
+            else{
+                this.emit(':tell', "Sorry, you can not close the session. Please enter a correct username. The active user in the system is " + this.attributes['logueado']);
+            }
+    },
+    'Read':function(){
+        //var myThis = this;
+        var usuarioLogueado = this.attributes['logueado'];
+        if(this.attributes['logueado']){
+            
+            func_db.obtener_datos_conf(usuarioLogueado,this.event.session.user.userId,(url, clase) =>{
+                if(url != null && clase != null){//Si existe el usuario
+                    if(url != "" && clase != ""){ //Si tiene url y clase definidos
+                        getNoticeResponse(url,clase, (cardTitle, speechOutput, repromptText, shouldEndSession) => {
+                                     //this.response = buildResponse(sessionAttributes, speechletResponse);
+                                     this.emit(':askWithCard', speechOutput,repromptText, cardTitle, speechOutput, null);
+                                  //this.emit(':saveState',true); //This is to persist session attributes into a table 'Users' in DynamoDB
+                                });
+                    }else{
+                       this.emit(':tell', "Sorry, the user "+ usuarioLogueado +" does not have url and class configured to get the content");
+                    }
+                }else{
+                 this.emit(':tell', "Sorry, there is no registered user with the name "+ usuarioLogueado +". Please, login with a valid username");
+                }
+            });//End obtener_datos_conf
+        }else{
+            this.emit(':tell', "Sorry, first you must login to be able to get news");
+        }
+    },
+    'Unhandled': function() {
+        this.emit(':tell', 'Sorry, I didn\'t get that.');
+    }
+};
+
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+                + ", sessionId=" + session.sessionId);
+}
+
+/**
+ * Called when the user ends the session ('saying exit' for example).
+ * Is not called when the skill returns shouldEndSession=true, because in this case the service doesn't receive a SessionEndedRequest.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+                + ", sessionId=" + session.sessionId);
+    // Add cleanup logic here
+}
+
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getNoticeResponse(url,clase,callback) {
+    
+    var repromptText = null;
+    var cardTitle = "Title - ";//aca iria el titulo de la noticia
+    //test http get
+    testGet(url,clase, (response,t) => {
+
+        var speechOutput = "The text of the article is: " + response; //response=body.data o parrafo 
+        var shouldEndSession = false;
+        cardTitle += t;
+
+        callback(cardTitle, speechOutput, repromptText, shouldEndSession);
+
+    });
+}
+
+function testGet(url,clase,responseFunction) {
+
+    
+    var EventEmitter = require("events").EventEmitter;
+    var func = new EventEmitter();
+    func.on('update', (parrafo,title) => {
+        //console.info('\n\nCall completed '+parrafo+" "+title); 
+        responseFunction(parrafo,title);
+    });
+    var request = require('request');
+    var xpath = require('xpath')
+    ,dom = require('xmldom').DOMParser;
+
+    request(url, (error, response, body) => { //url= 'http://cielosports.com/nota/75670/la_dirigencia_oferto_y_ahora_espera_por_el_si_de_ibanez/' 
+
+        var xml = body;
+        var doc = new dom().parseFromString(xml);
+        var parrafo = xpath.select("string(//p)", doc);
+        var title = xpath.select("string(//title)", doc);
+        //console.log(nodes[0].toString());
+      
+        func.emit('update',parrafo,title)
+    });
+    
+
+}//Cierra testGet
+
+
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+
+function onLaunch(url, clase, callback) {
+    /*console.log("onLaunch requestId=" + launchRequest.requestId
+                + ", sessionId=" + session.sessionId);
+    
+    // Dispatch to the skill.
+    getNoticeResponse(url,clase,callback);
+}
+*/
