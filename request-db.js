3c4e22e477bb64568918254fe085d8eefaadac9d
dssd <gonza.edelp77@gmail.com>
2019-11-06 20:44:28 -0300

    experimento funcionando

--- request-db.js
+++ request-db.js
@@ -1,89 +1,92 @@
-//Usar dynamoose
-var request = require('request');
 
-function obtener_datos_conf(loggedName,user_id,category,callback){ 
-  request({ method: 'GET', 
-            uri: 'https://alexa-apirest.herokuapp.com/users/notices/'+category+'/'+user_id+'/'+loggedName,
-            headers:{'Content-type': 'application/json'}
-          }, function (error, response, body) {
-              if (error) {console.log('error:', error)} // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              if(response.statusCode == 200){
-                var res = JSON.parse(body);
-                console.log('body:', res); 
-                callback(res) //res.url,res.xpath
-              }else{
-                callback(null)
-              }
-            }
-        );
+const fetch = require('node-fetch');
+
+const obtener_contenidos = async(loggedName,category) => {  //,user_id
+  const result = await fetch('https://alexa-apirest.herokuapp.com/users/contentsByCategory/'+category+'/'+loggedName) //+'/'+user_id
+  .then(res => res.json())
+  .then(json => {
+    return json //json=[{"infocontents": [{"_id","url","xpath","__v"}]},{}]
+  })
+  return result
+}
+ 
+const obtener_contenidos_por_orden = async(loggedName,conjunto) => {  //,user_id
+  const result = await fetch('https://alexa-apirest.herokuapp.com/users/contentsByOrder/'+conjunto+'/'+loggedName) //+'/'+user_id
+  .then(res => res.json())
+  .then(json => {
+    return json //json=[{content:{idcontent,infocontent:{url","xpath"},data}},{}] 
+  })
+  return result
 }
 
-function getCategories(loggedName,user_id,callback){
-    request({ method: 'GET', 
-            uri: 'https://alexa-apirest.herokuapp.com/users/categories/'+user_id+'/'+loggedName,
-            headers:{'Content-type': 'application/json'}
-          }, function (error, response, body) {
-              if (error) {console.log('error:', error)} // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              if(response.statusCode == 200){
-                var res = JSON.parse(body);
-                console.log('body:', res); //res=[{"category":"value"},{}]
-                callback(res) //res.url,res.xpath
-              }else{
-                callback(null)
-              }
-            }
-        );
-
+const getCategories = async(loggedName) => { //,user_id
+    const result = await fetch('https://alexa-apirest.herokuapp.com/users/categories/'+loggedName) //+user_id+'/'
+    .then(res => res.json())
+    .then(json => {                
+        console.log('body:', json); //json=[{"value"},{}]
+        return json //json.url,json.xpath
+    })
+    return result
 }
+
+const getConjuntos = async(loggedName) => {
+    const result = await fetch('https://alexa-apirest.herokuapp.com/users/flows/'+loggedName) //+user_id+'/'
+    .then(res => res.json())
+    .then(json => {                
+        console.log('body:', json); //json=[{"nombreConjunto":"value","pattern":"value"},{}]
+        return json 
+    })
+    return result  
+}
+
 //request to get a username from database
-function busqueda_usuario(loggedName,user_id,callback){
-  request({method: 'GET',
-           uri:'https://alexa-apirest.herokuapp.com/users/'+user_id+'/'+loggedName,
-           headers:{'Content-type': 'application/json'}
-          },(error, response, body) => { 
-            if(error){
-              console.log('error: '+ error)
-            }else{
-              console.log(body,Object.keys(response))
-              if(response.statusCode == 200){
-                console.log("usuario retornado: ",body)
-                callback(body)
-              } else {
-                console.log('error: '+ response.statusCode)
-                console.log(body)
-                callback(null)
-              }
-            }
-          }
-        );
+const busqueda_usuario = (loggedName) => {
+  return new Promise((resolve,reject) => {
+    const result = fetch('https://alexa-apirest.herokuapp.com/users/'+loggedName) //+user_id+'/'
+    .then(res => res.json())
+    .then(body => {
+      console.log(body)
+      resolve(body.name)
+    })
+    .catch(error => {
+      console.log("Error ",error)
+      reject(loggedName)
+    })
+  })
 }
 
-function registrar_usuario(username,user_id){ //HACER
-
-  request({method: 'POST',
-           uri:'https://alexa-apirest.herokuapp.com/users/',
-           headers:{'Content-type': 'application/json'},
-           body:{'name':username,'userId':user_id},
-           json: true
-          },(error, response, body) => { 
-            if(error){
-              console.log('error: '+ error)
-            }else{
-              console.log(response,Object.keys(response))
-              if(response.statusCode == 200){
-                console.log("usuario retornado: ",username)
-              } else {
-                console.log('error: '+ response.statusCode)
-                console.log(body)
-              }
-            }
-          }
-        );        
+const registrar_usuario = async(username) => { //,user_id
+  var body = { 'name':username}; //,'userId':user_id 
+  const result = await fetch('https://alexa-apirest.herokuapp.com/users/', { 
+      method: 'POST',
+      body:    JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+  })
+  .then(res => res.json())
+  .then(json => {
+    return json
+  })
+  return result
 }
+      
+//request to get title of content
+const getTitle = async(url,path) => { //,user_id
+  const result = await fetch("https://alexa-nightmare.herokuapp.com/contents/getTitle?url="+url+"&path="+path)
+  .then(res => res.text())
+  .then(body => {
+    console.log(body)
+    return body
+  })
+  return result
+}
+
+
 
 exports.busqueda_usuario = busqueda_usuario;
 exports.registrar_usuario = registrar_usuario;
-exports.obtener_datos_conf = obtener_datos_conf;
+exports.obtener_contenidos = obtener_contenidos;
+exports.obtener_contenidos_por_orden = obtener_contenidos_por_orden;
 exports.getCategories = getCategories;
+exports.getConjuntos = getConjuntos;
+
+exports.getTitle = getTitle;
